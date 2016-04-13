ec740c567f725950f12e554f4f796ac273544cb6
marisachao <marisa.chao@wustl.edu>
2016-04-13 18:04:43 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,15 +2,32 @@
 var correct = 0;
 
 // prompt
-var answer1 = prompt("ask a question...");
-console.log("the answer is:" + answer1);
+var answer1 = prompt("What is the town that the hit series Gilmore Girls takes place in?");
+//console.log("the answer is:" + answer1);
+var answer2 = prompt("What is the name of the high school Rory attends?")
+var answer3 = prompt("Who owns the diner that Lorelai and Rory frequently eat at?")
+var answer4 = prompt("What is the name of the inn taht Lorelai owns?")
+var answer5 = prompt("What is the name of Rory's first boyfriend")
+
 
 // if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+if ( answer1.toUpperCase() === 'STARS HOLLOW' ) {
+ correct += 1;
+}
+if ( answer2.toUpperCase() === 'CHILTON' ) {
+ correct += 1;
+}
+if ( answer3.toUpperCase() === 'LUKE' ) {
+ correct += 1;
+}
+if ( answer4.toUpperCase() === 'THE DRAGONFLY INN' ) {
+ correct += 1;
+}
+if ( answer5.toUpperCase() === 'DEAN' ) {
  correct += 1;
 }
 
-console.log("correct:" + correct);
+//console.log("correct:" + correct);
 
 // output results
 document.write("<p>" + answer1 + "</p>");
