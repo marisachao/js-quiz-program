095b74105db62ab548058cab14e753e2bcc78474
marisachao <marisa.chao@wustl.edu>
2016-04-13 18:21:27 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,28 +2,28 @@
 var correct = 0;
 
 // prompt
-var answer1 = prompt("What is the town that the hit series Gilmore Girls takes place in?");
+var answer1 = prompt("Who is the point guard of the Golden State Warriors?");
 //console.log("the answer is:" + answer1);
-var answer2 = prompt("What is the name of the high school Rory attends?")
-var answer3 = prompt("Who owns the diner that Lorelai and Rory frequently eat at?")
-var answer4 = prompt("What is the name of the inn that Lorelai owns?")
-var answer5 = prompt("What is the name of Rory's first boyfriend")
+var answer2 = prompt("What city is the Golden State Warriors' arena located in?")
+var answer3 = prompt("What country is Leandro Barbosa from?")
+var answer4 = prompt("What is the last name of the Warrior's head coach?")
+var answer5 = prompt("How many games did the Warriors win to start the 2015-16 season?")
 
 
 // if statement
-if ( answer1.toUpperCase() === 'STARS HOLLOW' ) {
+if ( answer1.toUpperCase() === 'STEPHEN CURRY' ) {
  correct += 1;
 }
-if ( answer2.toUpperCase() === 'CHILTON' ) {
+if ( answer2.toUpperCase() === 'OAKLAND' ) {
  correct += 1;
 }
-if ( answer3.toUpperCase() === 'LUKE' ) {
+if ( answer3.toUpperCase() === 'BRAZIL' ) {
  correct += 1;
 }
-if ( answer4.toUpperCase() === 'THE DRAGONFLY INN' ) {
+if ( answer4.toUpperCase() === 'KERR' ) {
  correct += 1;
 }
-if ( answer5.toUpperCase() === 'DEAN' ) {
+if ( answer5.toUpperCase() === '24' ) {
  correct += 1;
 }
 
@@ -43,13 +43,13 @@ document.write("<p>Congratulations, you got 3 questions correct! Bronze medal!!<
 }
 
 if ( correct === 2 ) {
-document.write("<p>You got 2 questions correct! Time to watch some Gilmore Girls and brush up on your knowledge!</p>");
+document.write("<p>You got 2 questions correct! You seem to know a litte bit about the Warriors</p>");
 }
 
 if ( correct === 1 ) {
-document.write("<p>You got 1 question correct! Time to watch some Gilmore Girls and brush up on your knowledge!?</p>");
+document.write("<p>You got 1 question correct! You're probably not much of a basketball fan, right?</p>");
 }
 
 if ( correct === 0 ) {
-document.write("<p>Sorry you didn't get any questions right. Have you even watched Gilmore Girls??</p>");
+document.write("<p>Sorry you didn't get any questions right.</p>");
 }
