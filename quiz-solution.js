24b50c15ab80bb12c3305a42c2a078234b2d7b76
marisachao <marisa.chao@wustl.edu>
2016-04-18 17:55:41 -0500

    Update quiz-solution.js

--- quiz-solution.js
+++ quiz-solution.js
@@ -2,26 +2,33 @@
 var correct = 0;
 
 // ask questions
-var answer1 = prompt("Name a programming language that's also a gem");
-if ( answer1.toUpperCase() === 'RUBY' ) {
+function question() {
+ //what our function will do
+ var answerInput = prompt("Name a programming language that's also a gem");
+ if ( answerInput.toUpperCase() === 'RUBY' ) {
  correct += 1; 
 }
-var answer2 = prompt("Name a programming language that's also a snake");
-if ( answer2.toUpperCase() === 'PYTHON' ) {
- correct += 1; 
-}
-var answer3 = prompt("What language do you use to style web pages?");
-if ( answer3.toUpperCase() === 'CSS' ) {
- correct += 1; 
-}
-var answer4 = prompt("What language do you use to build the structure of web pages?");
-if ( answer4.toUpperCase() === 'HTML' ) {
- correct += 1; 
-}
-var answer5 = prompt("What language do you use to add interactivity to a web page?");
-if ( answer5.toUpperCase() === 'JAVASCRIPT' ) {
- correct += 1; 
-}
+
+//var answer1 = prompt("Name a programming language that's also a gem");
+//if ( answer1.toUpperCase() === 'RUBY' ) {
+ //correct += 1; 
+//}
+//var answer2 = prompt("Name a programming language that's also a snake");
+//if ( answer2.toUpperCase() === 'PYTHON' ) {
+//correct += 1; 
+//}
+//var answer3 = prompt("What language do you use to style web pages?");
+//if ( answer3.toUpperCase() === 'CSS' ) {
+ //correct += 1; 
+//}
+//var answer4 = prompt("What language do you use to build the structure of web pages?");
+//if ( answer4.toUpperCase() === 'HTML' ) {
+ //correct += 1; 
+//}
+//var answer5 = prompt("What language do you use to add interactivity to a web page?");
+//if ( answer5.toUpperCase() === 'JAVASCRIPT' ) {
+ //correct += 1; 
+//}
 
 // output results
 document.write("<p>You got " + correct + " out of 5 questions correct.<p>");
